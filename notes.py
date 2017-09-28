6478f63aee87244c59c7106cabbf6f1dbab76dab
joetechem <joe@techemstudios.com>
2017-09-28 16:19:04 -0400

    after class notes

---
+++ notes.py
@@ -0,0 +1,10 @@
+# 9/28
+    # Got through all planned material
+    # i.e. tech em intro, what is coding/program, hardware vs. software,
+    # out-of-seat programming activity, efficiency, how comps manage
+    # info with 0 and 1, binary number system, explain lightbot, lightbot,
+    # discuss recognizing patterns and break problems down
+        # make binary number flash cards more lively
+        # make a bigger deal about patterns and decomposing problems
+
+# 10/5
