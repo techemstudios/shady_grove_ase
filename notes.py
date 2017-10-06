f05ffbe58b3f1fa5cb088eb43d1a6b9f13a118a4
joetechem <joe@techemstudios.com>
2017-10-06 14:50:02 -0400

    class notes

--- notes.py
+++ notes.py
@@ -14,6 +14,7 @@
         # 0 = off = no = False = closed
             # impromptu classroom door lesson: closed door is the gate, once 1 is reached, eletricity flows through causes something to happen vice versa.
             # impormptu relate logic gates to switches in the brain, neurons --> fight or flight
+                # think of our brains as containing all the programs, tells body what to do
     # design thinking discussion --> chair designer, fav games and why
     # design thinking questions --> hyperpad, 3 games, 4 minutes each
     # discussed good level design, started sketches of designs on paper
