b2bd7e35b0a474f94fd2ae5e619fdea698847587
joetechem <joe@techemstudios.com>
2017-10-20 10:42:08 -0400

    update notes.py

--- notes.py
+++ notes.py
@@ -18,3 +18,18 @@
     # design thinking discussion --> chair designer, fav games and why
     # design thinking questions --> hyperpad, 3 games, 4 minutes each
     # discussed good level design, started sketches of designs on paper
+
+# 10/12
+    # 5min final touches to level-design sketches
+    # review design thinking thoughts we thought about (I think)
+    # Hyperpad intro
+        # the layout
+        # background walkthrough
+        # stressed the importancance of recognizing everything is an object
+        # we can duplicate similar objects
+        # as a class: set a block's behaviors to a simple patrol pattern (x, y)
+        # as a class: control character movements (only joystick controlled/jump w/ button)
+
+# 10/19
+    # fine-tuned character movements
+    # set up interactions between player and environment i.e. obstacles, collectibles, etc.
